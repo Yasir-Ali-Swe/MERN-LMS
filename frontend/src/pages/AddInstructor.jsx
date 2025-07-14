b3124170c04986abdeaa2089f272ce9a08a438bb
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-14 15:08:06 +0500

    All App Pages

--- frontend/src/pages/AddInstructor.jsx
+++ frontend/src/pages/AddInstructor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AddInstructor = () => {
   return (
     <div>
-        <h1 className='text-3xl font-bold underline text-purple-800 text-center'>Add Instructor</h1>
+        <h1 className='lg:fixed top-0 w-full z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Add Instructor</h1>
     </div>
   )
 }
--- frontend/src/pages/ManageCourses.jsx
+++ frontend/src/pages/ManageCourses.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ManageCourses = () => {
   return (
     <div>
-        <h1 className='text-3xl font-bold underline text-purple-800 text-center'>Manage Courses</h1>
+        <h1 className='lg:fixed top-0 w-full z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Manage Courses</h1>
     </div>
   )
 }
