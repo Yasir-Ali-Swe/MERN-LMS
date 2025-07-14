9db81579436616acfb6a1796a1ee3da9bc783cda
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-14 14:39:35 +0500

    All Reusable Components

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,26 @@
 import Sidbar from "./Sidbar.jsx";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { RiMenuFold3Line } from "react-icons/ri";
+import { RiMenuFold4Line } from "react-icons/ri";
 
-const SidebarWidth=280;
+const SidebarWidth = 280;
 export default function Layout() {
+    const [open, setOpen] = useState(false);
+    const toggleSidebar = () => setOpen(!open);
     return (
         <div className="min-h-screen">
-            <div className={`fixed top-0 left-0 z-40 w-[${SidebarWidth}px]`}>
+            <div className={`fixed top-0 left-0 z-40 w-[${SidebarWidth}px] transition-transform duration-300 ease-in-out ${open ? "block" : "hidden"} lg:block`}>
                 <Sidbar />
             </div>
-            <main className="ml-[280px]">
+            <div className="lg:hidden fixed top-2 left-2 z-50">
+                <button onClick={toggleSidebar}>
+                    {
+                        open ? <RiMenuFold3Line size={28} className="bg-purple-800 text-white" /> : <RiMenuFold4Line size={28} className="text-purple-800" />
+                    }
+                </button>
+            </div>
+            <main className="lg:ml-[280px]">
                 <Outlet />
             </main>
         </div>
