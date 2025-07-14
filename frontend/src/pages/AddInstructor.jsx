9cc3bf2adf41c13c162016f701324de8c38abc15
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-14 15:16:17 +0500

    All App Pages

--- frontend/src/pages/AddInstructor.jsx
+++ frontend/src/pages/AddInstructor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AddInstructor = () => {
   return (
     <div>
-        <h1 className='lg:fixed top-0 w-full z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Add Instructor</h1>
+        <h1 className='fixed top-0 w-full lg:z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Add Instructor</h1>
     </div>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div className='h-[3000px]'>
-        <h1 className='lg:fixed top-0 w-full z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Dashboard</h1>
+        <h1 className='fixed top-0 w-full lg:z-50 py-2 text-center lg:text-left text-3xl font-bold bg-secondary text-textColor'>Dashboard</h1>
     </div>
   )
 }
