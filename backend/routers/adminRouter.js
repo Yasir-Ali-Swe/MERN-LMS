46552b489cfe31f3f85bd4bfe4da887e4c8048c2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-15 21:47:17 +0500

    Admin Routes

--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { AddInstructor, getAllUsers, deleteUser, updateUser } from "../controllers/adminController.js";
+import { AddInstructor, getAllUsers, deleteUser, updateUser, adminPublishCourse } from "../controllers/adminController.js";
+import { deleteCourse } from "../controllers/courseController.js";
 import { isAdmin } from "../middlewares/verifyIsAdmin.js";
 
 const router = express.Router();
 router.post("/addInstructor", isAdmin, AddInstructor);
 router.get("/getAllUsers", isAdmin, getAllUsers);
-router.delete("/deleteUser/:id", isAdmin, deleteUser);
+router.delete("/adminDeleteUser/:id", isAdmin, deleteUser);
+router.delete("/adminDeleteCourse/:id", isAdmin, deleteCourse);
 router.patch("/updateUser/:id", isAdmin, updateUser);
+router.patch("/adminPublishCourse/:id",isAdmin,adminPublishCourse)
 export default router;
