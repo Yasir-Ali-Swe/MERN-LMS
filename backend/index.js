2c72655ac0e0f3c7a3fbd26a1dd9001c58be7fef
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-15 14:10:42 +0500

    Entry Point

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,20 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import authRouter from "./routers/authRouter.js";
 
 dotenv.config();
 const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(cookieParser());
-const CONNECTION_STRING=process.env.CONNECTION_STRING;
-const PORT=process.env.PORT;
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const PORT = process.env.PORT;
 mongoose.connect(CONNECTION_STRING).then(() => {
     console.log("Database connected");
     server.listen(PORT, () => {
         console.log("Server started on port " + PORT);
     })
 })
+
+server.use("/auth", authRouter);
