247851026617def915f47dc70e68316c5249bc25
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-15 13:54:43 +0500

    Backend Configurations

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(cookieParser());
+const CONNECTION_STRING=process.env.CONNECTION_STRING;
+const PORT=process.env.PORT;
+mongoose.connect(CONNECTION_STRING).then(() => {
+    console.log("Database connected");
+    server.listen(PORT, () => {
+        console.log("Server started on port " + PORT);
+    })
+})
