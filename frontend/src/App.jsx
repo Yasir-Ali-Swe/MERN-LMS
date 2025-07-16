4d5567621f25a5c21515e6825021353cc494be21
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-16 20:20:20 +0500

    All app pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {  Login, Register, Dashboard , MyProfile, AddInstructor, ManageUsers, ManageCourses, ManagePost } from './pages'
+import { Login, Register, Dashboard, MyProfile, AddInstructor, ManageUsers, ManageCourses, ManagePost } from './pages'
 import Layout from './components/Layout.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import PublicRoute from './components/PublicRoute.jsx'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }>
           <Route index element={<Dashboard />} />
           <Route path="my-profile" element={<MyProfile />} />
           <Route path="add-instructor" element={<AddInstructor />} />
@@ -14,8 +20,8 @@ const App = () => {
           <Route path="manage-courses" element={<ManageCourses />} />
           <Route path="manage-post" element={<ManagePost />} />
         </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+        <Route path="login" element={<PublicRoute> <Login /></PublicRoute>} />
+        <Route path="register" element={<PublicRoute> <Register /></PublicRoute>} />
       </Routes>
     </BrowserRouter>
   )
