4e689d8a36819ea4edcee1875a1e63fb15b9e029
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-16 21:31:58 +0500

    Context Api for statmanagment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,12 +1,48 @@
-import { createContext } from "react";
-import { useState } from "react";
+import { createContext, use } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
-export const AuthProvider =({children})=>{
-    const [isAuthenticated,setIsAuthenticated]=useState(false);
-    return(
-        <AuthContext.Provider value={{isAuthenticated,setIsAuthenticated}}>
+export const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/auth/checkAuth", { withCredentials: true });
+                if (response.status === 200) {
+                    setIsAuthenticated(true);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        checkAuth();
+    })
+    const login = async ({ email, password }) => {
+        try {
+            const response = await axios.post("http://localhost:3000/auth/login", { email, password }, { withCredentials: true });
+            if (response.status === 200) {
+                setIsAuthenticated(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const logout = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/auth/logout", {}, { withCredentials: true });
+            console.log(response);
+            if (response.status === 200) {
+                setIsAuthenticated(false);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
